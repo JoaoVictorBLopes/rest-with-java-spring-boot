4df0d1bc74afa8b59c80b92962fb317f9c734955
Joao Victor Barboza Lopes <lopestitobarbosa@gmail.com>
2025-05-19 00:09:28 -0300

    padronização do nome dos endpoints, e implementação de migrations no projeto

--- rest-with-java-spring-boot/src/main/java/br/com/projetos/controller/TestLogController.java
+++ rest-with-java-spring-boot/src/main/java/br/com/projetos/controller/TestLogController.java
@@ -4,14 +4,16 @@ import br.com.projetos.services.PersonServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LoggTest {
+@RequestMapping("/api/test/v1")
+public class TestLogController {
 
     private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
-    @GetMapping("/test")
+    @GetMapping
     public String test () {
 
         logger.debug("Debug");
---
+++ rest-with-java-spring-boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(1,'São Paulo - Brasil','Ayrton','Male','Senna'),
+(2,'Hortolândia - São Paulo - Brasil','João Victor','Male','Barboza Lopes'),
+(5,'Hortolândia - São Paulo - Brasil','Fernanda','Female','do Prado Lopes'),
+(7,'Mvezo - South Africa','Nelson','Male','Mandela'),
+(8,'Smiljan - Croatia','Nikola','Male','Tesla');
